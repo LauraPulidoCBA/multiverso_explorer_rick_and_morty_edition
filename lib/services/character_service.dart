40a2586f4268a3b612b0e150af231385f67af00c
santiagobernal123 <santiagobarevalo05@gmail.com>
2026-03-19 17:43:34 -0500

    Consumo de API de Rick and Morty

---
+++ lib/services/character_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/character.dart';
+
+class CharacterService {
+  Future<List<Character>> fetchCharacters({String? status, int pages = 1}) async {
+    List<Character> allCharacters = [];
+
+    for (int i = 1; i <= pages; i++) {
+      String urlString = "https://rickandmortyapi.com/api/character?page=$i";
+
+      var url = Uri.parse(urlString);
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        List results = data["results"];
+      }
+    }
+
+    return allCharacters;
+  }
+}
