1b4a6af57929aeae5d51a9a2edcd8ece6612de26
herreracarrenoyeferso <herreracarreoyeferso@gmail.com>
2026-03-20 14:32:18 -0500

    lógica de favoritos

--- lib/provider/character_provider.dart
+++ lib/provider/character_provider.dart
@@ -2,9 +2,6 @@ class CharacterProvider extends ChangeNotifier {
   List<Character> _favourites = [];
   List<Character> get favourites => _favourites;
 }
-COMMIT 2
-git commit -m "lógica de favoritos"
-
 void toggleFavourite(Character character) {
   final exists = _favourites.any((c) => c.id == character.id);
 
