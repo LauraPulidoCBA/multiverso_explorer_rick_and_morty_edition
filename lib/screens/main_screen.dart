006fb1db1f7af2e5fbcce06883cad3ededd2d3cf
LauraPulidoCBA <laurapulido.cba.sena@gmail.com>
2026-03-19 21:51:04 -0500

    integración de pantallas

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -12,10 +12,18 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
 
+  final List<Widget> _pages = [
+    const SearchScreen(),
+    const FavouritesScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) => setState(() => _currentIndex = index),
